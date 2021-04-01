618f4c0e84e79675a1de3c01d11b5eb552ffb131
101006702 <wangchao8@huaqin.com>
2021-04-01 23:19:42 +0800

    [WXYFB-996]{type:charger; pro:K19A}K19A 33W charger bring up[1/2]
    [Problem]
    bring up
    
    [Solution]
    kernel配置
    
    [Test]
    验证pass
    
    [Comment]
    N/A
    
    Change-Id: If53ac55ba25433dc30b25170bf347d09aedc1149

--- drivers/power/supply/mediatek/battery/mtk_battery.c
+++ drivers/power/supply/mediatek/battery/mtk_battery.c
@@ -497,6 +497,12 @@ void otg_thermal_limit(void)
 		pr_err("primary_charger is NULL\n");
 #endif
 		primary_charger = get_charger_by_name("primary_chg");
+		
+		if (!primary_charger) {
+			pr_err("primary_charger is NULL again\n");
+			return;
+		}
+		pr_err("primary_charger is NULL0331\n");
 	}
 
 	if (otg_limit == 1) {
@@ -506,6 +512,7 @@ void otg_thermal_limit(void)
 	}
 }
 
+int get_charger_type(void);
 static int battery_get_property(struct power_supply *psy,
 	enum power_supply_property psp,
 	union power_supply_propval *val)
@@ -543,8 +550,10 @@ static int battery_get_property(struct power_supply *psy,
 		cycle_count = gm.bat_cycle;
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_TYPE:
-		charger_dev_get_charger_type(primary_charger, &type);
-		if (type > 3 || type < 0)
+		//charger_dev_get_charger_type(primary_charger, &type);
+		type = get_charger_type();
+		pr_err("ljj charger_dev_get_charger_type = %d\n",type);
+		if (type > 9 || type < 0)
 			type = 0;
 		val->intval = type;
 		break;
@@ -745,6 +754,10 @@ void battery_update(struct battery_data *bat_data)
 		pr_err("primary_charger is NULL\n");
 #endif
 		primary_charger = get_charger_by_name("primary_chg");
+		if (!primary_charger) {
+			pr_err("primary_charger is NULL00\n");
+			return;
+		}
 	}
 	charger_dev_is_charging_done(primary_charger, &chg_done);
 
@@ -4225,6 +4238,10 @@ static void otg_boost_limit_work(struct work_struct *work)
 		pr_err("primary_charger is NULL\n");
 #endif
 		primary_charger = get_charger_by_name("primary_chg");
+		if (!primary_charger) {
+			pr_err("primary_charger is NULL\n");
+			return;
+		}
 	}
 	if (otg_limit == 1) {
 		pr_err("phone is to high skip batterty otg boost check\n");
